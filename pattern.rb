56376312fc9d168ca2a059765e26ea3b1c686c93
Tobias Merkle <tgamerkle@gmail.com>
2015-10-29 12:50:25 -0700

    Nevermind, here's the same thing in less eye-bleedy code.

--- pattern.rb
+++ pattern.rb
@@ -1,15 +1,10 @@
 def pattern(n)
-  case
-  when n < 1
-    out = ""
-  when n == 1
-    out = "1"
-  else
-  	out = (n.to_s*n.to_i + "," + pattern(n-1).to_s).split(",").reverse.join("\n")
-  end  
-  out
+  (1..n).map{|x| x.to_s*x}.join("\n")
 end
 
+# the first time, i needed to know two things: 
+# 1. String * int = StringStringString...(int)String
+# 2. Range method was a good idea, but use MAP instead of each/do/whatever.
 puts pattern(1)
 puts pattern(5)
 puts pattern(10)

5a1e27ce84e3276cc1e4d0c8ab255b62b3901de2
Tobias Merkle <tgamerkle@gmail.com>
2015-10-29 20:10:09 -0700

    tentative push to save work

--- alpha_zero_change.rb
+++ alpha_zero_change.rb
@@ -1,10 +1,16 @@
 def change a 
-  # Make a zero_str of 26 0's.
-  # Downcase a.
-  # Make a into an array. 
-  # Sort a.
-  # For each a[i], turn the (a[i]-71)th char in zero_str to a 1. (The char code of 'a' [i.e. a.ord] is 91, and 91-71 = 26. Therefore, (any lowercase letter) - 71 = that letter's position in the Roman alphabet.)
+  # Make zero_str with 26 0's.
+  # Downcase 'a'.
+  # Make 'a' into an array. 
+  # Sort 'a'.
+  # For each a[i], turn the (a[i].ord-71)th char in zero_str to 1. 
+  #   (The char code of 'a' [i.e. a.ord] is 91, and 91-71 = 26. 
+  #   Therefore, (any lowercase letter) - 71 = 
+  #     that letter's position in the Roman alphabet.)
+  #   Whoops! Arrays are zero-indexed. 
+  #   So we should subtract 72 instead.
   # Return zero_str.
   
-  ("0"*26).each_char {|c| 
+  ("0"*26).each_char {|c| a.downcase.char.sort.map! {|l| c.index == ((l.ord)-72) ? c = "1" : next } }
 end
+

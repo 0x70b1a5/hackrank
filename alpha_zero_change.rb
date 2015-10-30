2859a6ad1d5fe362bf432973fb75677bdb571f94
Tobias Merkle <tgamerkle@gmail.com>
2015-10-29 19:23:54 -0700

    Beginnings of a pretty cool thing

---
+++ alpha_zero_change.rb
@@ -0,0 +1,10 @@
+def change a 
+  # Make a zero_str of 26 0's.
+  # Downcase a.
+  # Make a into an array. 
+  # Sort a.
+  # For each a[i], turn the (a[i]-71)th char in zero_str to a 1. (The char code of 'a' [i.e. a.ord] is 91, and 91-71 = 26. Therefore, (any lowercase letter) - 71 = that letter's position in the Roman alphabet.)
+  # Return zero_str.
+  
+  ("0"*26).each_char {|c| 
+end

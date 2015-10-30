01e81bc3a8e77d5d7f20204281eddc269256b362
Tobias Merkle <tgamerkle@gmail.com>
2015-10-29 23:33:02 -0700

    temp push with half done alt soln

--- alpha_zero_change.rb
+++ alpha_zero_change.rb
@@ -3,14 +3,14 @@ def change a
   # Downcase 'a'.
   # Make 'a' into an array. 
   # Sort 'a'.
-  # For each a[i], turn the (a[i].ord-71)th char in zero_str to 1. 
-  #   (The char code of 'a' [i.e. a.ord] is 91, and 91-71 = 26. 
-  #   Therefore, (any lowercase letter) - 71 = 
-  #     that letter's position in the Roman alphabet.)
-  #   Whoops! Arrays are zero-indexed. 
-  #   So we should subtract 72 instead.
+  # For each a[i], turn the (a[i].ord-96)th char in zero_str to 1. 
+  #   (The char code of 'a' [i.e. a.ord] is 97, and 97-96 = 1. 
+  #   Therefore, (any lowercase letter) - 96 = 
+  #   that letter's position in the Roman alphabet.)
+  # Whoops, make that 97 because zero-index.
   # Return zero_str.
   
-  ("0"*26).each_char {|c| a.downcase.char.sort.map! {|l| c.index == ((l.ord)-72) ? c = "1" : next } }
+  (("0"*26).split("").map.with_index {|c, i| a.downcase.chars.sort.map {|l| i == ((l.ord)-97) ? c = "1" : next } }).join("")
 end
 
+puts change("butts")
